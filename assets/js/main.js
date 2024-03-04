6ee22332f9f037d80e33900e64d100b0df456225
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-04 13:20:42 +0100

    Add cicle for loop to iteraate through the list of object Add for-in loop to print the values of each single object in console (details of member of the team)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,52 @@
 console.log('Hello, team!');
+
+//  - Copio l'array "team" nel file js
 const team = [
     {
-      name: 'Wayne Barnett',
-      role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
-      name: 'Angela Caroll',
-      role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
     },
     {
-      name: 'Walter Gordon',
-      role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
-      name: 'Angela Lopez',
-      role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
-      name: 'Scott Estrada',
-      role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
     },
     {
-      name: 'Barbara Ramos',
-      role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
-  ];
+];
+
+//  - Inserisco l'array di ogetti in un ciclo for
+//      - Ogni oggetto dell'array lo passo nel ciclo for-in
+//          - Stampo in console i valori dell'oggetto
+
+for (let i = 0; i < team.length; i++) {
+    const member = team[i];
+    // console.log(member);
+
+    for (const key in member) {
+        console.log(member[key]);
+    };
+    
+}
+
+   
+    
